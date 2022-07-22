4957dcd50dbf8e73ec0aec7d251c1863f4d0ccfa
tranth0ngquang <77227285+tranth0ngquang@users.noreply.github.com>
2022-07-22 22:28:16 +0700

    wow

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1610270288,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/background-sheet0.png",
+		"images/planeblue-sheet0.png",
+		"images/planeblue-sheet1.png",
+		"images/planeblue-sheet2.png",
+		"images/groundsnow-sheet0.png",
+		"images/groundrock-sheet0.png",
+		"images/rockice-sheet0.png",
+		"images/rockicedown-sheet0.png",
+		"images/pufflarge-sheet0.png",
+		"images/stargold-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
